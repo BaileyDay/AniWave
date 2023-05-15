ab9cb523fafbdc901b23ca6e1a9ba6e73525980a
BaileyDay <bailey.j.day@gmail.com>
2023-05-15 14:43:42 -0400

    sitemap added

---
+++ pages/api/sitemap.ts
@@ -0,0 +1,57 @@
+import { SitemapStream, streamToPromise } from 'sitemap'
+import { getAllPosts, getAllPagesSlugs } from '../../lib/sanity.client'
+let sitemap: string
+
+export default async function handler(req, res) {
+  res.setHeader('Content-Type', 'application/xml')
+
+  if (sitemap) {
+    res.write(sitemap)
+    res.end()
+    return
+  }
+
+  try {
+    const smStream = new SitemapStream({
+      hostname: 'https://www.aniwave.io/api/sitemap',
+    })
+
+    const posts = await getAllPosts()
+    const pages = await getAllPagesSlugs()
+
+    posts.forEach((post) => {
+      smStream.write({
+        url: `/posts/${post.slug}`,
+        changefreq: 'weekly',
+        priority: 0.9,
+      })
+    })
+
+    pages.forEach((page) => {
+      smStream.write({
+        url: `/${page.slug.current}`,
+        changefreq: 'monthly',
+        priority: 0.8,
+      })
+    })
+
+    // smStream.write({ url: '/about', changefreq: 'monthly', priority: 0.7 })
+
+    streamToPromise(smStream).then((value) => {
+      sitemap = value.toString()
+    })
+
+    smStream.end()
+
+    streamToPromise(smStream).then((sm) => {
+      sitemap = sm.toString()
+    })
+
+    smStream.pipe(res).on('error', (e) => {
+      throw e
+    })
+  } catch (e) {
+    console.error(e)
+    res.status(500).end()
+  }
+}
