21389f52b0b42340113145471317ecd092076bb1
BaileyDay <bailey.j.day@gmail.com>
2023-05-16 15:54:21 -0400

    working on og images

--- components/PostPageHead.tsx
+++ components/PostPageHead.tsx
@@ -11,7 +11,6 @@ export interface PostPageHeadProps {
 
 export default function PostPageHead({ settings, post }: PostPageHeadProps) {
   const title = settings.title ?? demo.title
-
   const ogImageUrl = post.coverImage?.asset?._ref
     ? urlForImage(post.coverImage).url()
     : undefined
@@ -19,14 +18,23 @@ export default function PostPageHead({ settings, post }: PostPageHeadProps) {
   return (
     <Head>
       <title>{post.title ? `${post.title} | ${title}` : title}</title>
+      <meta name="description" content={post.excerpt} />
+      {/* Facebook Meta Tags */}
+      {/* <meta property="og:url" content={post.url} /> */}
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={post.title} />
+      <meta property="og:description" content={post.excerpt} />
+      {ogImageUrl && <meta property="og:image" content={ogImageUrl} />}
+
+      {/* Twitter Meta Tags */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content="aniwave.io" />
+      {/* <meta property="twitter:url" content={post.url} /> */}
+      <meta name="twitter:title" content={post.title} />
+      <meta name="twitter:description" content={post.excerpt} />
+      {ogImageUrl && <meta name="twitter:image" content={ogImageUrl} />}
+
       <BlogMeta />
-      {ogImageUrl && (
-        <>
-          <meta property="og:image" content={ogImageUrl} />
-          <meta property="og:title" content={post.title} />
-          <meta property="og:description" content={post.excerpt} />
-        </>
-      )}
     </Head>
   )
 }
