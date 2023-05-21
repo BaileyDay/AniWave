be821ae9b9db30f251b55d0c8f9f25279fd600fe
BaileyDay <bailey.j.day@gmail.com>
2023-05-21 19:30:34 -0400

    favicon work

--- components/BlogMeta.tsx
+++ components/BlogMeta.tsx
@@ -3,26 +3,5 @@
  */
 
 export default function BlogMeta() {
-  return (
-    <>
-      <meta name="viewport" content="width=device-width,initial-scale=1.0" />
-      <link rel="apple-touch-icon" sizes="180x180" href="public/favicon.ico" />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="public/favicon.ico"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="public/favicon.ico"
-      />
-
-      <link rel="shortcut icon" href="public/favicon.ico" />
-      <meta name="msapplication-TileColor" content="#000000" />
-      <meta name="theme-color" content="#000" />
-    </>
-  )
+  return <></>
 }
