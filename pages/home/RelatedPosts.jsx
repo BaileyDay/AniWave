de101f1ce679a4663d4fbf920219165128c8195b
BaileyDay <bailey.j.day@gmail.com>
2023-05-25 14:00:29 -0400

    working on deploy errors

--- pages/home/RelatedPosts.jsx
+++ pages/home/RelatedPosts.jsx
@@ -2,7 +2,6 @@ import { urlForImage } from 'lib/sanity.image'
 import Link from 'next/link'
 
 const RelatedPosts = ({ posts }) => {
-  console.log(posts)
   return (
     <div className="bg-slate-1 py-24 text-slatedark-12 dark:bg-slatedark-1 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
@@ -17,13 +16,13 @@ const RelatedPosts = ({ posts }) => {
         <div className="mx-auto mt-16 grid max-w-2xl auto-rows-fr grid-cols-1 gap-8 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
           {posts.map((post) => (
             <article
-              key={post.id}
+              key={post?.id}
               className="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl bg-slate-12 px-8 pb-8 pt-80 sm:pt-48 lg:pt-80"
             >
               <Link href={`/posts/${post?.slug}`}>
                 <img
                   src={urlForImage(post?.coverImage).url()}
-                  alt={post.title}
+                  alt={post?.title}
                   className="absolute inset-0 -z-10 h-full w-full object-cover"
                 />
                 <div className="absolute inset-0 -z-10 bg-gradient-to-t from-slate-12 via-slate-12/40" />
@@ -39,7 +38,7 @@ const RelatedPosts = ({ posts }) => {
                         day: 'numeric',
                       })}
                     >
-                      {new Date(post.date).toLocaleDateString('en-US', {
+                      {new Date(post?.date).toLocaleDateString('en-US', {
                         year: 'numeric',
                         month: 'long',
                         day: 'numeric',
@@ -50,16 +49,16 @@ const RelatedPosts = ({ posts }) => {
                     <div className="flex gap-x-2.5">
                       <img
                         src={urlForImage(post?.author.picture).url()}
-                        alt={post.author.name}
+                        alt={post?.author.name}
                         className="bg-white/10 h-6 w-6 flex-none rounded-full"
                       />
-                      {post.author.name}
+                      {post?.author.name}
                     </div>
                   </div>
                 </div>
                 <h3 className="text-white mt-3 text-lg font-semibold leading-6">
                   <span className="absolute inset-0" />
-                  {post.title}
+                  {post?.title}
                 </h3>
               </Link>
             </article>
