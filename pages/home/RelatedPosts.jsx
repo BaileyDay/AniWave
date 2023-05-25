f7cda9f333834fb3214ee689e9e974f2efa4ab32
BaileyDay <bailey.j.day@gmail.com>
2023-05-25 15:05:38 -0400

    tryin anotha fix

--- pages/home/RelatedPosts.jsx
+++ pages/home/RelatedPosts.jsx
@@ -15,7 +15,7 @@ const RelatedPosts = ({ posts }) => {
           </h3>
         </div>
         <div className="mx-auto mt-16 grid max-w-2xl auto-rows-fr grid-cols-1 gap-8 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-          {posts.map((post) => (
+          {posts?.map((post) => (
             <article
               key={post?._id}
               className="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl bg-slate-12 px-8 pb-8 pt-80 sm:pt-48 lg:pt-80"
