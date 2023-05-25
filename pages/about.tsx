78a8570b75ee358654b03516920133666f212ed5
BaileyDay <bailey.j.day@gmail.com>
2023-05-25 14:41:46 -0400

    working on errors

---
+++ pages/about.tsx
@@ -0,0 +1,13 @@
+import GlobalFooter from 'components/common/GlobalFooter'
+import GlobalNav from 'components/common/GlobalNav'
+
+const AboutPage = () => {
+  return (
+    <>
+      <GlobalNav />
+      <GlobalFooter />
+    </>
+  )
+}
+
+export default AboutPage
